87ed2332bf13db3b124780eaeeee30b064d4e421
SnmLogic <snmlogic111@gmail.com>
2022-01-16 16:59:22 +0530

    Download page is not complete

--- src/Components/Download/DownloadMain.js
+++ src/Components/Download/DownloadMain.js
@@ -2,47 +2,26 @@ import React from "react";
 
 export default function DownloadMain() {
   return (
-    <div>
-      <section className="text-gray-400 bg-gray-900 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -mx-4 -mb-10 text-center">
-            <div className="sm:w-1/2 mb-10 px-4">
-              <div className="rounded-full h-40 overflow-hidden">
-                <img
-                  alt="testimonial"
-                  className="w-40 h-40 p-2 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-800 bg-white"
-                  src="https://th.bing.com/th/id/OIP.7s2t4hDFEq6d1dL7ItHiWgAAAA?pid=ImgDet&rs=1"
-                />
-              </div>
-              <h2 className="title-font text-2xl font-medium text-white mt-6 mb-3">
-                For Windows
-              </h2>
-              <a
-                href="https://github.com/Abdesol/CutCode/releases/download/v3.0.0/CutCode.v3.0.0.Win.x64.exe"
-                className="flex mx-auto mt-6 text-white bg-indigo-500 w-min border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded"
-              >
-                Download
-              </a>
-            </div>
-            <div className="sm:w-1/2 mb-10 px-4">
-              <div className="rounded-lg h-40 overflow-hidden">
-                <img
-                  alt="testimonial"
-                  className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-800 bg-gray-800 bg-opacity-10"
-                  src="https://th.bing.com/th/id/OIP.7FcBoMFn-VZkzqC6bmfptgHaHa?pid=ImgDet&rs=1"
-                />
-              </div>
-              <h2 className="title-font text-2xl font-medium text-white mt-6 mb-3">
-                For Linux
-              </h2>
-
-              <a
-                href="https://github.com/Abdesol/CutCode/releases/download/v3.0.0/CutCode_linux_x64_installer.sh"
-                className="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded w-min"
-              >
-                Download
-              </a>
-            </div>
+    <div className="bg-gray-900 h-screen">
+      <section className="container pt-20 md:flex item-center justify-center">
+        <div className="version py-5 grid place-items-center w-auto">
+          <img
+            src="https://cdn.discordapp.com/icons/929333388312182784/4bbb906e5ad2a8af6d76ec8c2f0633b7.webp?size=128"
+            alt="CutCode Logo"
+            className="w-30 h-48"
+          />
+          <div className="about-version text-gray-300 text-xl pl-2">
+            <h1 className="font-poppins">Version: 3.0.0</h1>
+            <h1 className="font-poppins">Build: 2.32.23</h1>
+            <h1 className="font-poppins">8 January 2022</h1>
+          </div>
+        </div>
+        <div className="download-main flex flex-col align-center justify-center bg-red-700">
+          <h1 className="font-poppins text-5xl text-white font-semibold px-10">
+            Download CutCode
+          </h1>
+          <div className="download">
+            <h1>Windows</h1>
           </div>
         </div>
       </section>
--- src/Pages/Download.js
+++ src/Pages/Download.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./../Components/Download/Header";
-import Main from "./../Components/Download/Main";
 import Footer from "./../Components/Home/Footer";
 import DownloadMain from "./../Components/Download/DownloadMain";
 
@@ -9,7 +8,6 @@ export default function Download() {
   return (
     <div>
       <Header />
-      <Main />
       <DownloadMain />
       <Footer />
     </div>
